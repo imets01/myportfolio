4500f33b093f766f76926f4b65f241439ef12790
imets01 <100612206+imets01@users.noreply.github.com>
2024-11-27 12:43:01 +0100

    apos change

--- components/WavyDemo.jsx
+++ components/WavyDemo.jsx
@@ -13,8 +13,8 @@ export function WavyBackgroundDemo() {
             <br />I am <span>Akos Imets</span>
           </h1>
           <p className="text-2xl mb-8 max-w-2xl ">
-            A Master's student in Informatics at UZH, with a strong software
-            development background.
+            A Master&apos;s student in Informatics at UZH, with a strong
+            software development background.
           </p>
           <a href="/Akos-Imets-CV.pdf" download>
             <button className="px-8 py-2 rounded-lg bg-primary-foreground text-primary font-bold transition duration-200 hover:bg-primary hover:text-primary-foreground border-2 border-transparent hover:border-primary-foreground">
