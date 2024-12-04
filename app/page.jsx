7c576e6a82ac51cfdfcabf20519bf33d0de74723
imets01 <100612206+imets01@users.noreply.github.com>
2024-12-04 18:59:42 +0100

    analytics

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,10 @@
 import Portfolio from "@/components/portfolio";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
     <div>
+      <Analytics />
       <Portfolio />
     </div>
   );
